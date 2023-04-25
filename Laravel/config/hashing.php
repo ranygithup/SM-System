052e9db0eb02b31f1d199371cadd2611aa6c1c2b
vectorasoft <vectorasoft.dev@gmail.com>
2023-04-25 23:16:09 +0700

    first commit

---
+++ Laravel/config/hashing.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'driver' => 'bcrypt',
+    'bcrypt' => [
+        'rounds' => env('BCRYPT_ROUNDS', 10),
+    ],
+
+    'argon' => [
+        'memory' => 65536,
+        'threads' => 1,
+        'time' => 4,
+    ],
+];
