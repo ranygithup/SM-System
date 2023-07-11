98aaf4df059e6e3e100ccb00fdec48a626e715e0
Dara-Coder <darasmart2020@gmail.com>
2023-07-12 01:25:27 +0700

    update ui

---
+++ public/assets/javascript/index.js
@@ -0,0 +1,27 @@
+let head = document.head;
+
+let cdn_style = [
+    'assets/css/bootstrap.css'
+];
+
+let cdn_script = [
+    'assets/javascript/script.js',
+    'assets/javascript/bootstrap.js'
+];
+
+cdn_style.map(link => {
+    let styleTag = document.createElement('link');
+    styleTag.setAttribute('rel','stylesheet');
+    styleTag.setAttribute('type','text/css');
+    styleTag.setAttribute('href',document.baseURI+link);
+    head.appendChild(styleTag);
+});
+
+cdn_script.map(link => {
+    let scriptTag = document.createElement('script');
+    scriptTag.setAttribute('type','text/javascript');
+    scriptTag.setAttribute('src',document.baseURI+link);
+    head.appendChild(scriptTag);
+});
+
+document.scripts[0].remove();
---
+++ resources/views/master.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <title>System For Testing</title>
+        <script type="text/javascript" src="{{ asset('assets/javascript/index.js') }}"></script>
+    </head>
+    <body>
+    </body>
+</html>
