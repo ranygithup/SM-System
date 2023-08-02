749c86075657007039c86d81311e27f8560c3a4d
Dara-Coder <darasmart2020@gmail.com>
2023-08-02 21:14:56 +0700

    update ui

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,70 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\DB;
-use App\Models\SaveImage;
 use App\Models\Book;
-use Exception;
 
 class BookController extends Controller
 {
     function save(Request $req){
-        $rules = [
-            'id' => 'numeric',
-            'name' => 'required|string|min:4|max:30',
-            'description' => 'string|max:250',
-            'program_id' => 'required|numeric',
-            'department_id' => 'required|numeric',
-            'photo' => 'string',
-            'created_at' => 'string',
-            'updated_at' => 'string'
-        ];
-
-        $validator = Validator::make($req->all(),$rules);
-        if($validator->fails()){
-            return response()->json([
-                'status' => 200,
-                'error_message' => $validator->messages()
-            ]);
-        }
-        else{
-            try{
-                $data = $req->input();
-                $book = new Book();
-                $book->name = $data['name'];
-                $book->description = $data['description'];
-                $book->program_id = $data['program_id'];
-                $book->department_id = $data['department_id'];
-                $book->photo_file_name = SaveImage::saveImage('images',$data['photo']);
-                unset($data['photo']);
-                $book->save();
-                
-                return response()->json([
-                    'status' => 200,
-                    'data' => 'Book Added'
-                ]);
-            }
-            catch(Exception $e){
-                return response()->json([
-                    'status' => 500,
-                    'error_message' => 'Something went wrong'
-                ]);
-            }
-        }
+        return Book::save($req->all());
     }
 
     function list(){
-        $rows = DB::table('book as b')->join('main_program as m','m.id','=','b.program_id')->join('department as d','d.id','=','b.department_id')->selectRaw('b.id,b.name,b.description,b.photo_file_name,m.name as program,d.name as department')->get();
-
-        foreach($rows as $row){
-            $row->image_url = SaveImage::getImage('images',$row->photo_file_name);
-            unset($row->photo_file_name);
-        }
-        
-        return response()->json([
-            'status' => 200,
-            'data' => $rows
-        ]);
+        return Book::list();
     }
 }
--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,16 +2,62 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Exception;
 
-class Book extends Model
-{
-    protected $table = 'book';
-    protected $fillable = [
-        'name',
-        'description',
-        'program_id',
-        'department_id',
-        'photo_file_name'
-    ];
+class Book{
+    protected $tbl = 'book';
+    
+    static function save($data){
+        $rules = [];
+
+        $validator = Validator::make($data,$rules);
+        if($validator->fails()){
+            return response()->json([
+                'status' => 200,
+                'error_message' => $validator->messages()
+            ]);
+        }
+        else{
+            try{
+                if($data['id'] > 0){
+                    DB::update('UPDATE book SET ');
+                }
+                else{
+                    $name = $data['name'];
+                    $description = $data['description'];
+                    $program_id = $data['program_id'];
+                    $department_id = $data['department_id'];
+                    $photo_file_name = SaveImage::saveImage('images',$data['photo']);
+                    unset($data['photo']);
+                    DB::insert('INSERT INTO $this->tbl (`name`)');
+                }
+                
+                return response()->json([
+                    'status' => 200,
+                    'data' => 'Book Added'
+                ]);
+            }
+            catch(Exception $e){
+                return response()->json([
+                    'status' => 500,
+                    'error_message' => 'Something went wrong'
+                ]);
+            }
+        }
+    }
+
+    static function list(){
+        $rows = DB::table('book as b')->join('main_program as m','m.id','=','b.program_id')->join('department as d','d.id','=','b.department_id')->selectRaw('b.id,b.name,b.description,b.photo_file_name,m.name as program,d.name as department')->get();
+        foreach($rows as $row){
+            $row->image_url = SaveImage::getImage('images',$row->photo_file_name);
+            unset($row->photo_file_name);
+        }
+        
+        return response()->json([
+            'status' => 200,
+            'data' => $rows
+        ]);
+    }
 }
