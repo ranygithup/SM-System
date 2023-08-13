4994c0fd8e0dd9bb6408838635deb6c353348f80
Dara-Coder <darasmart2020@gmail.com>
2023-08-13 12:40:29 +0700

    update ui

--- app/ScriptManagement/ScriptProvider.php
+++ app/ScriptManagement/ScriptProvider.php
@@ -9,11 +9,8 @@
 
             $scripts_load = array(
                 'https://code.jquery.com/jquery-3.7.0.min.js',
-                'https://code.jquery.com/ui/1.13.2/jquery-ui.js',
                 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js',
                 'https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js',
-                'https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js',
-                'https://cdn.jsdelivr.net/npm/chart.js',
                 'https://cdn.jsdelivr.net/npm/sweetalert2@11'
             );
 
